6fd6db21c16f78833650323ca411d7c6231f1c61
David <davidchedrick@gmail.com>
2021-12-29 13:25:22 -0600

    commit tree e4e1e7940a74cc1f2a810acdc3b159ea6ac37f29

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,22 +1,77 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+
+  const [stocks, setStocks] = useState([]);
+  const [portfolio, setPortfolio] = useState([]);
+  const [filter, setFilter] = useState("Tech");
+  const [sortBy, setSortBy] = useState("Alphabetically")
+  console.log("sortBy: ", sortBy);
+  console.log("filter: ", filter);
+  console.log("portfolio: ", portfolio);
+  console.log("stocks: ", stocks);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+    .then(resp => resp.json())
+    .then(setStocks)
+  }, [])
+
+  function handleAddStock(stockToAdd) {
+    const stockInPort = portfolio.find(
+      stock => stock.id === stockToAdd.id
+    );
+    if(!stockInPort) {
+      setPortfolio([...portfolio, stockToAdd])
+    }
+  }
+
+  function handleRemoveStock(stockToRemove) {
+    setPortfolio(portfolio => 
+      portfolio.filter(stock => stock.id !== stockToRemove.id)
+    );
+  }
+
+  const sortedStocks = [...stocks].sort((stock1, stock2) => {
+    if(sortBy === "Alphabetically") {
+      return stock1.name.localeCompare(stock2.name);
+    } else {
+      return stock1.price - stock2.price;
+    }
+  });
+
+  const filteredStocks = sortedStocks.filter(
+    stock => stock.type === filter
+  );
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar 
+        sortBy={sortBy}
+        onChangeSort={setSortBy}
+        filterBy={filter}
+        onChangeFilter={setFilter}
+      />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer 
+            stocks={filteredStocks} 
+            onStockClick={handleAddStock}
+          />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer 
+            stocks={portfolio}
+            onRemoveStock={handleRemoveStock}
+          />
         </div>
       </div>
     </div>
   );
+
 }
 
 export default MainContainer;
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,13 +1,23 @@
 import React from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer({ stocks, onStockClick }) {
+
+  const stockCards = stocks.map(stock => (
+    <Stock 
+      key={stock.id}
+      stock={stock}
+      onStockClick={onStockClick}
+    />
+  ))
+
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {stockCards}
     </div>
   );
+
 }
 
 export default StockContainer;
